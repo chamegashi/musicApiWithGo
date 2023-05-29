a864d6a8e23841e51d67601d7148d8a6ea8afd1b
chamegashi <msymsy0693wrb@gmail.com>
2023-05-30 00:06:55 +0900

    update codes

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"net/http"
 )
 
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	hello := []byte("hello")
+	_, err := w.Write(hello)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	fmt.Println(time.Now())
+	http.HandleFunc("/test", testHandler)
+	fmt.Println("server started on http://localhost:8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module example.com/m/v2
 
 go 1.20
+
+require google.golang.org/protobuf v1.30.0
+
+require github.com/google/go-cmp v0.5.9 // indirect
